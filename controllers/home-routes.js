eb9a3d188f1f0e5254559e608c3560bd7d040444
James Hunter <jamessahunter@gmail.com>
2023-11-06 14:02:35 -0800

    add login functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -7,4 +7,24 @@ router.get('/', async (req, res)=> {
     res.render('homepage', {projects} );
 });
 
+
+router.get('/login', (req,res)=>{
+res.render('login');
+})
+
+router.get('/profile', async (req,res)=>{
+    const profileData= await Project.findAll({
+        where: {
+            user_id: req.session.user_id,
+        }
+    })
+    const profile = profileData.map((profile)=> profile.get({plain:true}));
+    res.render('profile',{profile})
+})
+
+router.get('*',async (req, res)=> {
+    const dbProjects= await Project.findAll();
+    const projects = dbProjects.map((project) => project.get({plain: true}));
+    res.render('homepage', {projects} );
+})
 module.exports = router;
